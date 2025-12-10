fec61ca6e1066db7b71c92eaa5d2bbf82fb77f27
Alexandrhall <alexandrhall@gmail.com>
2025-12-10 22:18:42 +0100

    react update

--- clientapp/src/components/Second.tsx
+++ clientapp/src/components/Second.tsx
@@ -2,6 +2,11 @@ const Second = () => {
   return (
     <div>
       <h1>Second</h1>
+      <div>
+        <h2>
+          Check out <a href="/swagger" target="_blank" rel="noopener noreferrer">/swagger</a> or other backend path <a href="/WeatherForecast" target="_blank" rel="noopener noreferrer">/weatherforecast</a>
+        </h2>
+      </div>
     </div>
   );
 };
